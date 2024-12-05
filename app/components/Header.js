962a180406b8f9f036a7fd8427f0b0bd92f8c3e8
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-12-05 19:55:20 +0100

    Add - Github icon and link. Refactor - btn animations

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,11 +5,11 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="sticky top-0 bg-black p-2">
-      <nav className="grid gap-12 sm:grid-cols-4 grid-cols-2 justify-items-stretch">
+    <header className="sticky top-0 bg-black p-2 ml-2 mr-2">
+      <nav className="grid gap-12 sm:grid-cols-5 grid-cols-2 justify-items-stretch">
         <Link
           href="/pages/projects"
-          className="hover:translate-x-2 transition duration-500 ease-in-out hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
+          className="transition duration-500 ease-in-out hover:scale-105 hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
         >
           <Image
             aria-hidden
@@ -21,7 +21,7 @@ const Header = () => {
           Projects
         </Link>
         <a
-          className="hover:translate-x-2 transition duration-500 ease-in-out hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
+          className="transition duration-500 ease-in-out hover:scale-105 hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
           href="https://uneedevs.vercel.app/"
           target="_blank"
           rel="noopener noreferrer"
@@ -37,7 +37,7 @@ const Header = () => {
         </a>
         <Link
           href="/pages/about"
-          className="hover:translate-x-2 transition duration-500 ease-in-out hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
+          className="transition duration-500 ease-in-out hover:scale-105 hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5 "
         >
           <Image
             aria-hidden
@@ -58,6 +58,20 @@ const Header = () => {
             aria-hidden
             src="/icons/linkedin.png"
             alt="Window icon"
+            width={28}
+            height={28}
+          />
+        </a>
+        <a
+          className="transition duration-500 ease-in-out hover:scale-105 hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center bg-orange-400 text-background gap-2 hover:bg-blue-300 hover:text-white text-sm sm:text-base h-10 sm:h-9 px-4 sm:px-5"
+          href="https://github.com/rhiz0m"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            aria-hidden
+            src="/icons/github.png"
+            alt="Github icon"
             width={28}
             height={28}
           />
