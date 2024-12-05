83896734ff61a30055d6ea5274b904bc91d9af9a
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-12-05 15:42:05 +0100

    Fix -responsivness issues

--- app/components/Header.js
+++ app/components/Header.js
@@ -6,7 +6,7 @@ import Link from "next/link"
 const Header = () => {
   return (
     <header className="sticky top-0 bg-black p-2">
-      <nav className="flex gap-12">
+      <nav className="grid gap-12 sm:grid-cols-4 grid-cols-2 justify-items-stretch">
         <Link
           href="/pages/projects"
           className="hover:translate-x-2 transition duration-500 ease-in-out hover:underline hover:underline-offset-4 rounded-md border border-solid border-transparent flex items-center justify-center text-blue-300 bg-foreground text-background gap-2 hover:bg-slate-800 dark:hover:bg-[#ccc] hover:text-blue-100 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 "
--- app/components/Main.js
+++ app/components/Main.js
@@ -4,20 +4,20 @@ const Main = () => {
   return (
     <main className="flex-grow flex items-center justify-center max-w-2xl mx-auto overflow-y-auto mt-12">
       <div className="bg-white p-4 rounded-md">
-        <h1 className="text-2xl font-bold text-center">Portfolio</h1>
-        <div className="flex p-2">
-          <div className="flex-[1]">
+        <h1 className="text-2xl font-bold text-center p-2">Portfolio</h1>
+        <div className="grid sm:grid-cols-3 grid-cols-1 gap-4 items-center justify-center">
+          <div className="flex justify-center">
             <Image
-            className="rounded-full border-e-red-50 "
+              className="rounded-full p-2 object-cover"
               aria-hidden
               src="/images/andreas.jpg"
               alt="profile icon"
-              width={300}
-              height={300}
               layout="responsive"
-            ></Image>
+              width={1}
+              height={1}
+            />
           </div>
-          <p className="flex-[2] ml-8">
+          <p className="sm:col-span-2">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
