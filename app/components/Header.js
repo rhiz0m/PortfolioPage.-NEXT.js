41c63fd52f5fb745ed7928603f818050c5476992
Andreas Antonsson <antonsson.andreas@hotmail.com>
2025-01-10 11:00:03 +0100

    Fix - correcting the link adresses

--- app/components/Header.js
+++ app/components/Header.js
@@ -16,11 +16,11 @@ const internal = [
 const external = [
   {
     icon: faGithub,
-    url: "https://github.com",
+    url: "https://github.com/rhiz0m",
   },
   {
     icon: faLinkedin,
-    url: "https://www.linkedin.com",
+    url: "https://www.linkedin.com/in/andreas-antonsson-12209024b/",
   },
 ]
 
